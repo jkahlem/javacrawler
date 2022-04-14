0a2c047596c4501b1974361c9fcc0cdfe41cefc9
unknown <kahle.julian@gmail.com>
2022-04-15 05:09:28 +0900

    * print stack traces to error message

--- src/main/java/com/returntypes/crawler/RpcErrorResolver.java
+++ src/main/java/com/returntypes/crawler/RpcErrorResolver.java
@@ -1,5 +1,8 @@
 package com.returntypes.crawler;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.lang.reflect.Method;
 import java.util.List;
 
@@ -11,9 +14,19 @@ public class RpcErrorResolver implements ErrorResolver {
 
     @Override
     public JsonError resolveError(Throwable t, Method method, List<JsonNode> arguments) {
+        ErrorData errorData = new ErrorData(t.toString(), getStackTrace(t)); 
         if (t.getMessage() == null) {
-            return new ErrorResolver.JsonError(JsonError.INTERNAL_ERROR.code, "An unexpected error occurred: " + t.toString(), new ErrorData("", ""));
+            return new ErrorResolver.JsonError(JsonError.INTERNAL_ERROR.code,
+                "An unexpected error occurred: " + t.toString() + "\n Java Stacktrace: \n" + getStackTrace(t),
+                errorData);
         }
-        return new ErrorResolver.JsonError(JsonError.INTERNAL_ERROR.code, t.getMessage(), new ErrorData("", ""));
+        return new ErrorResolver.JsonError(JsonError.INTERNAL_ERROR.code, t.getMessage(), errorData);
+    }
+
+    private String getStackTrace(Throwable throwable) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        throwable.printStackTrace(pw);
+        return sw.toString();
     }
 }
